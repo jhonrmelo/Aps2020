8b189c693c9b276db4d09dc637487d5412637609
jhonrmelo <45439356+jhonrmelo@users.noreply.github.com>
2020-04-07 22:15:18 -0300

    Creation of the project

---
+++ APS2020/Views/PaginaInicial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace APS2020.Views
+{
+    public partial class PaginaInicial : Form
+    {
+        public PaginaInicial()
+        {
+            InitializeComponent();
+        }
+    }
+}
